e4c80fb98c892fe4964ca00e76e2541bdc18f68d
NarenSiddharth <narensiddharth@gmail.com>
2024-09-23 10:59:16 +0530

    added setup page

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'main.dart';
+import 'setup.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -81,13 +82,12 @@ class _HomePageState extends State<HomePage> {
     Map<String, dynamic>? data = await _userService.getUserDetails();
     setState(() {
       userData = data;
-      isLoading = false;  // Data loaded, stop loading
+      isLoading = false; // Data loaded, stop loading
     });
   }
 
   @override
-  Widget build(BuildContext context) 
-  {
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(children: [
         Container(
@@ -105,7 +105,7 @@ class _HomePageState extends State<HomePage> {
           child: Row(
             children: [
               if (isLoading)
-                const CircularProgressIndicator()  // Show loading indicator while data is being fetched
+                const CircularProgressIndicator() // Show loading indicator while data is being fetched
               else if (userData != null && userData!['name'] != null)
                 Text(
                   "Hello ${userData!['name']}!",
@@ -124,78 +124,70 @@ class _HomePageState extends State<HomePage> {
                     color: Colors.black,
                   ),
                 ),
-
-                const SizedBox(width: 40),
-
-                IconButton(
-                  icon: const Icon(Icons.notifications, color: Colors.black),
-                  onPressed: () {
+              const SizedBox(width: 40),
+              IconButton(
+                icon: const Icon(Icons.notifications, color: Colors.black),
+                onPressed: () {
                   // Handle bell icon press
-                  },
+                },
+              ),
+              Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black,
+                      blurRadius: 5,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
                 ),
-
-                Container(
-                  width: 40,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.black,
-                        blurRadius: 5,
-                        offset: Offset(0, 2),
-                      ),
-                    ],
-                  ),
-                  child: Center(
-                    child: Text(
-                      userData != null && userData!['initial'] != null
-                          ? userData!['initial']
-                          : '',
-                      style: GoogleFonts.inter(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
+                child: Center(
+                  child: Text(
+                    userData != null && userData!['initial'] != null
+                        ? userData!['initial']
+                        : '',
+                    style: GoogleFonts.inter(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
                     ),
                   ),
                 ),
+              ),
 
-               /* IconButton(
+              /* IconButton(
                   icon: const Icon(Icons.person, color: Colors.black, size: 30,),
                   onPressed: () {
                     //Navigator.pushNamed(context, "ProfilePage");
                   },
                 ), */
             ],
-
           ),
-
-
         ),
-        
         Positioned(
           top: 250,
-          left: MediaQuery.of(context).size.width / 2 - 100, // Center horizontally
+          left: MediaQuery.of(context).size.width / 2 -
+              100, // Center horizontally
           child: Container(
             width: 220,
             height: 100,
-            
             child: Center(
               child: Text(
-          'Start your insulin journey with us...',
-          style: GoogleFonts.inter(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-            color: Colors.grey[800],
-          ),
-          textAlign: TextAlign.center,
+                'Start your insulin journey with us...',
+                style: GoogleFonts.inter(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[800],
+                ),
+                textAlign: TextAlign.center,
               ),
             ),
           ),
         ),
-
         Center(
           child: ElevatedButton(
             onPressed: () {
@@ -209,7 +201,7 @@ class _HomePageState extends State<HomePage> {
             style: ElevatedButton.styleFrom(
               backgroundColor: Colors.greenAccent, // Background color
               shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12), // Rounded corners
+                borderRadius: BorderRadius.circular(12), // Rounded corners
               ),
               padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
             ),
@@ -218,70 +210,64 @@ class _HomePageState extends State<HomePage> {
               height: 49, // Set the width to 248 px
               alignment: Alignment.center,
               child: Row(
-              mainAxisAlignment: MainAxisAlignment.center, // Align the text at the center
-              children: [
-                Text(
-                "C H A N G E   Y O U R  ",
-                style: GoogleFonts.inter(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-                ),
-                Text(
-                "L I F E !",
-                style: GoogleFonts.inter(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-                ),
-              ],
+                mainAxisAlignment:
+                    MainAxisAlignment.center, // Align the text at the center
+                children: [
+                  Text(
+                    "C H A N G E   Y O U R  ",
+                    style: GoogleFonts.inter(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    "L I F E !",
+                    style: GoogleFonts.inter(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
               ),
             ),
-
-
-
           ),
         ),
-
-        
         Positioned(
           top: MediaQuery.of(context).size.height * 0.85,
-          left: MediaQuery.of(context).size.width / 2 - 50, // Center horizontally
+          left:
+              MediaQuery.of(context).size.width / 2 - 50, // Center horizontally
           child: ElevatedButton(
             onPressed: () {
               Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(builder: (context) => LoginPage()),
-                        );
+                context,
+                MaterialPageRoute(builder: (context) => LoginPage()),
+              );
             },
             style: ElevatedButton.styleFrom(
- // Background color
+              // Background color
               shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15), // Rounded corners
+                borderRadius: BorderRadius.circular(15), // Rounded corners
               ),
               padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
             ),
             child: Text(
               "Log Out",
               style: GoogleFonts.inter(
-          fontSize: 14,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
               ),
             ),
           ),
         ),
-
         Positioned(
           bottom: 20,
           left: 20,
           right: 20,
           child: Container(
             padding: const EdgeInsets.all(16.0),
-
-            
             child: Text(
               'Let us help you in making your life easier...',
               style: GoogleFonts.inter(
@@ -293,12 +279,10 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
         )
-
       ]),
     );
   }
 }
-
 
 class Homepage2 extends StatefulWidget {
   const Homepage2({super.key});
@@ -306,8 +290,6 @@ class Homepage2 extends StatefulWidget {
   @override
   _Homepage2State createState() => _Homepage2State();
 }
-
-
 
 class _Homepage2State extends State<Homepage2> {
   final TextEditingController weightController = TextEditingController();
@@ -343,7 +325,7 @@ class _Homepage2State extends State<Homepage2> {
         decoration: InputDecoration(
           prefixIcon: Icon(icon, color: Colors.black, size: 18.0),
           labelText: labelText,
-          labelStyle: GoogleFonts.inter(color: Colors.black), 
+          labelStyle: GoogleFonts.inter(color: Colors.black),
           focusedBorder: const OutlineInputBorder(
             borderSide: BorderSide(color: Colors.greenAccent, width: 1.0),
           ),
@@ -352,7 +334,6 @@ class _Homepage2State extends State<Homepage2> {
           ),
           filled: true,
           fillColor: Colors.white,
-
         ),
       ),
     );
@@ -387,98 +368,121 @@ class _Homepage2State extends State<Homepage2> {
     Map<String, dynamic>? data = await _userService.getUserDetails();
     setState(() {
       userData = data;
-      isLoading = false;  // Data loaded, stop loading
+      isLoading = false; // Data loaded, stop loading
     });
   }
 
   @override
-  Widget build(BuildContext context) 
-  {
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [
-        Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/images/background.png"),
-              fit: BoxFit.cover,
-            ),
+        body: Stack(children: [
+      Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/background.png"),
+            fit: BoxFit.cover,
           ),
         ),
-
-        Container(
+      ),
+      Container(
           width: 338,
           height: 45,
           margin: const EdgeInsets.only(top: 40, left: 15),
-          child : Row(children: [
-            IconButton(
-              icon: const Icon(Icons.arrow_back, color: Colors.black),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-            Expanded(
-              child: Center(
-              child: Text(
-                "Start your insulin journey",
-                style: GoogleFonts.inter(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-                color: Colors.black,
+          child: Row(
+            children: [
+              IconButton(
+                icon: const Icon(Icons.arrow_back, color: Colors.black),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              Expanded(
+                child: Center(
+                  child: Text(
+                    "Start your insulin journey",
+                    style: GoogleFonts.inter(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black,
+                    ),
+                  ),
                 ),
               ),
-              ),
-            ),
-    
-          ],)
-        ),
-
-        Positioned(
-          top: 200,
-          left: 50, // Center horizontally
-          child: Container(
-            width: 270,
-            height: 25,
-            
-            
-            
-              child: Text(
-                'Help Us Get to Know You!',
-                style: GoogleFonts.inter(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black,
-                ),
-              
+            ],
+          )),
+      Positioned(
+        top: 200,
+        left: 50, // Center horizontally
+        child: Container(
+          width: 270,
+          height: 25,
+          child: Text(
+            'Help Us Get to Know You!',
+            style: GoogleFonts.inter(
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+              color: Colors.black,
             ),
           ),
         ),
-        Positioned(
-          top: MediaQuery.of(context).size.height * 0.26, // Adjust the top position as needed
-          left: MediaQuery.of(context).size.width * 0.07, // Adjust the left position as needed
-          right: MediaQuery.of(context).size.width * 0.07, // Adjust the right position as needed
-          child: Container(
-            padding: const EdgeInsets.all(16.0),
-            
-            child: Column(
-              children: [
-          const SizedBox(height: 20.0),
-          _buildTextField(weightController, 'Weight (in cm)', Icons.monitor_weight, keyboardType: TextInputType.number),
-          const SizedBox(height: 16.0),
-          _buildTextField(heightController, 'Height (in cm)', Icons.height, keyboardType: TextInputType.number),
-              ],
+      ),
+      Positioned(
+        top: MediaQuery.of(context).size.height *
+            0.26, // Adjust the top position as needed
+        left: MediaQuery.of(context).size.width *
+            0.07, // Adjust the left position as needed
+        right: MediaQuery.of(context).size.width *
+            0.07, // Adjust the right position as needed
+        child: Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 20.0),
+              _buildTextField(
+                  weightController, 'Weight (in cm)', Icons.monitor_weight,
+                  keyboardType: TextInputType.number),
+              const SizedBox(height: 16.0),
+              _buildTextField(heightController, 'Height (in cm)', Icons.height,
+                  keyboardType: TextInputType.number),
+            ],
+          ),
+        ),
+      ),
+      Positioned(
+        top: MediaQuery.of(context).size.height *
+            0.90, // Adjust the top position as needed
+        left: MediaQuery.of(context).size.width *
+            0.07, // Adjust the left position as needed
+        right: MediaQuery.of(context).size.width *
+            0.07, // Adjust the right position as needed
+        child: ElevatedButton(
+          onPressed: () {
+            // Handle continue button press
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      const SetupPage()), // Replace NextPage with your next page widget
+            );
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.greenAccent, // Background color
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12), // Rounded corners
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+          ),
+          child: Text(
+            "CONTINUE",
+            style: GoogleFonts.inter(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              letterSpacing: 5.0,
             ),
           ),
         ),
-
-        
-        
-        
-
-
-
-      ]
-      )
-    );
-
+      ),
+    ]));
   }
 }
